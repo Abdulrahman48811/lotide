6f5f77900bf47ba9efea0c282ed9f3917f48ab04
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-11-24 19:26:32 +0000

    if one element of both arrays arent equal, push the filtered array

---
+++ without.js
@@ -0,0 +1,22 @@
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+};
+
+const without = function(array1, array2) {
+  let filteredArray = [];
+  for (const element of array1) {
+    if (!array2.includes(element)) {
+      filteredArray.push(element);
+    }
+  }
+  return filteredArray;
+};
+
+module.exports = without;
+
+console.log(without([1, 2, 3], [1]))
+console.log(without(["1", "2", "3"], [1, 2, "3"]))
