f6885c4e89ceda2dde5635508442ae09a3cfae54
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-11-24 23:46:46 +0000

    add one/+1 to each letter of the sentance

---
+++ countLetters.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+
+const countLetters = function(LHL){
+  let str = LHL.split(" ").join(" ");
+  const result = {};
+  for(let letter of LHL){
+    if(letter != " "){
+      if(result[letter]){
+        result[letter] += 1
+      } else {
+        result[letter] = 1
+      }
+    }
+  
+}
+console.log(result);
+  return result;
+}
+
+countLetters("lighthouse in the house");
