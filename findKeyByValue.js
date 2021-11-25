2ebcf38b7c659415c342a35093a91a49f7a2b4ec
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-11-25 18:24:15 +0000

    created a function

---
+++ findKeyByValue.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  return (actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
+};
+
+
+const findKeyByValue = function (obj, value) {
+  for (let key of Object.keys(obj)) {
+    if (obj[key] === value){
+      return key;
+    }
+  }
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
