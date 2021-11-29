6c19b72ad5685b3f7140be3190ae2ec82ca36093
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-11-29 21:06:55 +0000

    refactoring assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,5 +1,4 @@
-  const eqArrays = require("./eqArrays");
-
+const eqArrays = require("./eqArrays");
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅  Assertion passed: ${array1} === ${array2}`);
