0ca003c891f1b9af7ba00450fe953fb50f2ee935
Abdulrahman Mohammed <abdulrahman48811@gmail.com>
2021-12-02 18:11:22 +0000

    implementing with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,24 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require("../middle")
+const assert = require('chai').assert;
+const middle = require('../middle');
 
 
-assertArraysEqual(middle([1, 2, 3]), [2])
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [1])
-assertArraysEqual(middle([1, 2, 3, 4]), [2] )
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4])
 
+
+
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require("../middle")
+
+
+// assertArraysEqual(middle([1, 2, 3]), [2])
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [1])
+// assertArraysEqual(middle([1, 2, 3, 4]), [2] )
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [4])
+
+// describe("#head", () => {
+//   it("returns 1 for [1, 2, 3]", () => {
+//     assert.strictEqual(head([1, 2, 3]), 1);
+//   });
+//   it("returns '5' for ['5']", () => {
+//     assert.strictEqual(head(['5']), '5');
+//   });
+// });
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,20 @@
-const assertEqual = require('../assertEqual');
+const assertEqual = require('chai').assertEqual;
 const tail = require("../tail")
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
+tail(words);// no need to capture the return value since we are not checking it
+tail(words.length, 3)
+
+// assertEqual(words.length, 3);
+
+
+// describe("#head", () => {
+//   it("returns 1 for [1, 2, 3]", () => {
+//     assert.strictEqual(head([1, 2, 3]), 1);
+//   });
+//   it("returns '5' for ['5']", () => {
+//     assert.strictEqual(head(['5']), '5');
+//   });
+// });
+
+
